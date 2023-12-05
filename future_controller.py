a5610e19bf85924f8ee2bbc7310acc715c222913
Ankit Kumar Pandey <ankitkpandey1@gmail.com>
2023-12-05 12:41:33 +0530

    add ch4

--- future_controller.py
+++ future_controller.py
@@ -1,4 +1,8 @@
+from typing import Annotated
 from litestar import get, Controller
+
+from litestar.openapi.spec.example import Example
+from litestar.params import Parameter, Body
 
 future_maps = {
     1: "future1",
@@ -7,9 +11,17 @@ future_maps = {
     4: "future4",
 }
 
-class FutureController(Controller):
 
+class FutureController(Controller):
     @get(path="/{future_id:int}")
-    async def get_future_id_status(self, future_id: int) -> str:
+    async def get_future_id_status(
+        self,
+        future_id: Annotated[
+            int,
+            Parameter(
+                description="The ID of the future to retrieve",
+                ge=0,
+            ),
+        ],
+    ) -> str:
         return "tt"
- 
